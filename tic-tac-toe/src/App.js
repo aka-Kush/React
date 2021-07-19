0818926bcabc2770bb3228ef5dee76a2382f3e0d
aka-Kush <kushivansh27@gmail.com>
2021-07-19 13:56:51 +0530

    added Styling to components

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Board from './components/Board';
+import "./styles/root.scss";
 
 function App() {
   return (
-    < >
+    <div className="app">
       <h1>TIC TAC TOE</h1>
       <Board />
-    </>
+    </div>
   );
 }
 
