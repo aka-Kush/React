e03532949c0c9538e392f1fd63d079a39dc70340
aka-Kush <kushivansh27@gmail.com>
2021-07-20 18:01:57 +0530

    added State sharing

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,7 +1,28 @@
+import React, { useState } from 'react';
 import Board from './components/Board';
 import "./styles/root.scss";
 
 function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setXNext] = useState(false);
+
+  const handleSquareClick = (position) => {
+
+      if (board[position]) { return;}
+
+      setBoard((prevState) => {
+          return prevState.map((square, pos) => {
+              if (pos === position) {
+                  return isXNext ? 'X' : 'O';  
+              }
+              return square;
+          });
+      });
+
+      setXNext ( (prevState) => !prevState)
+  }
+
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
--- tic-tac-toe/src/components/Board.js
+++ tic-tac-toe/src/components/Board.js
@@ -1,27 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Square from './Square'
 
 const Board = () => {
-    const [board, setBoard] = useState(Array(9).fill(null));
-    const [isXNext, setXNext] = useState(false);
-
-    const handleSquareClick = (position) => {
-
-        if (board[position]) { return;}
-
-        setBoard((prevState) => {
-            return prevState.map((square, pos) => {
-                if (pos === position) {
-                    return isXNext ? 'X' : 'O';  
-                }
-                return square;
-            });
-        });
-
-        setXNext ( (prevState) => !prevState)
-    }
     
-
     const renderSquare = (position) => {
         return (
             <Square
