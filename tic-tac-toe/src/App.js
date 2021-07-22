20ee1ce06c1df06b29a84b7a17c14d0346d038f9
aka-Kush <kushivansh27@gmail.com>
2021-07-22 20:21:25 +0530

    added Winner calculation using State sharing

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
+import {calculateWinner} from './helpers.js';
 import "./styles/root.scss";
 
 function App() {
@@ -7,9 +8,17 @@ function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [isXNext, setXNext] = useState(false);
 
+  const winner = calculateWinner(board);
+
+  console.log(winner);
+
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${isXNext ? 'X' : 'O'}`;
+
   const handleSquareClick = (position) => {
 
-      if (board[position]) { return;}
+      if (board[position] || winner) { return;}
 
       setBoard((prevState) => {
           return prevState.map((square, pos) => {
@@ -26,6 +35,7 @@ function App() {
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
+      <h2>{message}</h2>
       <Board board={board} handleSquareClick={handleSquareClick}/>
     </div>
   );
