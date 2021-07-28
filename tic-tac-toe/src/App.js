710ad44b7f4164d8a35455b3858752b6f5bee4a8
aka-Kush <kushivansh27@gmail.com>
2021-07-28 13:54:34 +0530

    added Start New Game button && highlighted Winning Squares

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -5,16 +5,17 @@ import StatusMessage from './components/StatusMessage';
 import {calculateWinner} from './helpers.js';
 import "./styles/root.scss";
 
-function App() {
+const NEW_GAME = [
+  { board: Array(9).fill(null), isXNext: true }
+]
 
-  const [history, setHistory] = useState([
-    { board: Array(9).fill(null), isXNext: true },
-  ]);
+const App = () => {
+  const [history, setHistory] = useState(NEW_GAME);
 
   const [currentMove, setCurrentMove] = useState(0);
   const current = history[currentMove];
 
-  const winner = calculateWinner(current.board);
+  const {winner, winningSquares} = calculateWinner(current.board);
 
   const handleSquareClick = (position) => {
 
@@ -41,11 +42,17 @@ function App() {
     setCurrentMove(move);
   }
 
+  const onNewGame = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  }
+
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <StatusMessage winner={winner} current={current}/>
-      <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <Board board={current.board} handleSquareClick={handleSquareClick} winningSquares={winningSquares}/>
+      <button type="button" onClick={onNewGame}>Start new game</button>
       <History history={history} moveTo={moveTo} currentMove={currentMove}/>
     </div>
   );
