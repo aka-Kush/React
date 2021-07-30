9da06771fbf4ba933f97187294c04f35585bef81
aka-Kush <kushivansh27@gmail.com>
2021-07-30 14:25:14 +0530

    added Corner edges styling

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -56,7 +56,8 @@ const App = () => {
         Start new game
       </button>
       <h2 style={{fontWeight: 'normal'}}>Current game history</h2>
-      <History history={history} moveTo={moveTo} currentMove={currentMove}/>
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
+      <div className="bg-balls"/>
     </div>
   );
 }
