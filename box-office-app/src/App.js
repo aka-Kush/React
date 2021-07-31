4cf1ea295994086d8c5830ef1c6920695b178a08
aka-Kush <kushivansh27@gmail.com>
2021-07-31 15:13:34 +0530

    added Navbar and created page components

--- box-office-app/src/App.js
+++ box-office-app/src/App.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Starred from "./pages/Starred";
 
 function App() {
   return (
-    <Switch>
+    <div>
+      <Nav />
 
-      <Route exact path="/">
-        This is home page
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/starred">
-        This is starred
-      </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-      <Route>
-        This is 404 page
-      </Route>
+        <Route>
+          <div>404 NOT FOUND</div>
+        </Route>
+      </Switch>
 
-    </Switch>
+    </div>
   );
 }
 
---
+++ box-office-app/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            This is a HOME page
+        </div>
+    )
+}
+
+export default Home
