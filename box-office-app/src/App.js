5639282a8494132e5a9cacde44a99caf3288512b
aka-Kush <kushivansh27@gmail.com>
2021-07-31 14:14:45 +0530

    added react-router

--- box-office-app/src/App.js
+++ box-office-app/src/App.js
@@ -1,9 +1,23 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <Switch>
+
+      <Route exact path="/">
+        This is home page
+      </Route>
+
+      <Route exact path="/starred">
+        This is starred
+      </Route>
+
+      <Route>
+        This is 404 page
+      </Route>
+
+    </Switch>
   );
 }
 
