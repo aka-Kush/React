c47d2332b264cff6f3e25801fe63ee330e0aa8da
aka-Kush <kushivansh27@gmail.com>
2021-07-31 15:42:22 +0530

    created input and added fetch functionality from API

--- box-office-app/src/pages/Home.js
+++ box-office-app/src/pages/Home.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
 const Home = () => {
+
+    const [input, setInput] = useState('');
+
+    const onInputChange = (event) => {
+        setInput(event.target.value);
+    }
+
+    const onSearch = () => {
+        // https://www.tvmaze.com/api
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
+
+    const onKeyDown = (event) => {
+        if (event.keyCode === 13) {
+            onSearch();
+        };
+    }
+
     return (
         <MainPageLayout>
-            This is a HOME page
+            <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+            <button type="button" onClick={onSearch}>Search</button>
         </MainPageLayout>
     )
 }
