99c48ddd7dab16baab654019f330d726bbfc6a6c
aka-Kush <kushivansh27@gmail.com>
2021-08-02 16:18:07 +0530

    added loading and error messages

--- box-office-app/src/pages/Show.js
+++ box-office-app/src/pages/Show.js
@@ -5,19 +5,39 @@ import { apiGet } from '../misc/config';
 const Show = () => {
     const { id } = useParams();
     const [show, setShow] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
     
     useEffect(() => {
+
+        let isMounted = true;
+
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-            .then(results => setShow(results))
+            .then(results => {
+                if (isMounted) {
+                    setShow(results);
+                    setIsLoading(false);
+                }
+            }).catch(err => {
+                if (isMounted) {
+                    setError(err.message);
+                    setIsLoading(false);
+                }
+            })
+        return () => { isMounted = false; }
     }, [id]);
 
     console.log(show);
 
-    return (
-        <div>
-            this is show page
-        </div>
-    )
+    if (isLoading) {
+        return <div>Data is being loaded..</div>
+    };
+
+    if (error) {
+        return <div>Error occured : {error}</div>
+    }
+
+    return <div> this is show page </div>
 }
 
 export default Show;
