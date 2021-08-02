7db626ab7c6242edd48a4cd5aa62ad195f4e7217
aka-Kush <kushivansh27@gmail.com>
2021-08-02 17:01:39 +0530

    created show page components and added data to them

--- box-office-app/src/pages/Show.js
+++ box-office-app/src/pages/Show.js
@@ -1,5 +1,9 @@
 import React, {useReducer, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
 
 const reducer = (prevState, action) => {
@@ -53,8 +57,6 @@ const Show = () => {
         return () => { isMounted = false; }
     }, [id]);
 
-    console.log(show);
-
     if (isLoading) {
         return <div>Data is being loaded..</div>
     };
@@ -63,7 +65,36 @@ const Show = () => {
         return <div>Error occured : {error}</div>
     }
 
-    return <div> this is show page </div>
+    return (
+        <div>
+            <ShowMainData
+                image={show.image}
+                name={show.name}
+                rating={show.rating}
+                summary={show.summary}
+                tags={show.genres}
+            />
+
+            <div>
+                <h2>Details</h2>
+                <Details
+                    status={show.status}
+                    network={show.network}
+                    premiered={show.premiered}
+                />
+            </div>
+
+            <div>
+                <h2>Seasons</h2>
+                <Seasons seasons={show. _embedded.seasons}/>
+            </div>
+
+            <div>
+                <h2>Cast</h2>
+                <Cast cast={show._embedded.cast}/>
+            </div>
+        </div>
+    )
 }
 
 export default Show;
