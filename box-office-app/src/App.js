4043046f7248b7fc6723eb8d7b20b8ed80a95b7d
aka-Kush <kushivansh27@gmail.com>
2021-08-03 16:01:46 +0530

    added styling to nav and title using styled components

--- box-office-app/src/App.js
+++ box-office-app/src/App.js
@@ -4,9 +4,19 @@ import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Show from "./pages/Show";
 import Starred from "./pages/Starred";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
       <Switch>
         <Route exact path="/">
           <Home />
@@ -24,6 +34,7 @@ function App() {
           <div>404 NOT FOUND</div>
         </Route>
       </Switch>
+    </ThemeProvider>
   );
 }
 
